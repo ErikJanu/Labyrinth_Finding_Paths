2c5bde147ea35564298440cf34330b1ab6ea3999
Erik <erik.janusch@yahoo.com>
2023-10-26 12:07:30 +0200

    Phase 1 Completed (print_labyrinth prints the individual lines of the labyrinth ands adds information about the rows and the columns at the top, the bottom, the left and the right of the labyrinth)

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 def print_labyrinth(lab: list[str]):
-    print(lab)
+    columns_counter = len(lab[0])
+    columns_counter_string = " "
+    for i in range(1, columns_counter+1):
+        columns_counter_string += str(i)
+        if i == columns_counter+1:
+            columns_counter_string += " "
+    lab.insert(0, columns_counter_string)
+    lab.append(columns_counter_string)
+    for index in range(1, len(lab)-1):
+        lab[index] = str(index) + lab[index] + str(index)
+    for line in lab:
+        print(line)
 
 
 if __name__ == '__main__':
-    print_labyrinth("Test")
+    space = chr(32)
+    wall = chr(219)
+    print_labyrinth([wall + wall + wall + wall + wall + wall + wall,
+                     wall + wall + wall + wall + wall + space + wall,
+                     wall + wall + wall + wall + wall + space + wall,
+                     wall + wall + space + space + space + space + wall,
+                     wall + wall + space + wall + wall + wall + wall,
+                     wall + wall + space + space + wall + wall + wall,
+                     wall + space + space + wall + wall + wall + wall,
+                     wall + space + space + space + space + wall + wall,
+                     wall + wall + wall + wall + wall + wall + wall,
+                    ])
 
