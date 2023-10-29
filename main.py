9d1867378459e6091e058add900c8c11e0aa33b8
Erik <erik.janusch@yahoo.com>
2023-10-29 15:48:29 +0100

    Clean up code, follow best-practices (use enumerate when needed, use f-strings, try out .join() , simplify if-statements with list instead of or) While lopp added to ask for user input until a traversable, valid location is entered by the user as start and end point

--- main.py
+++ main.py
@@ -3,23 +3,21 @@ from collections import deque
 
 def print_labyrinth(lab: list[str], path: list[tuple[int, int]] = None):
     if path is not None:
-        for line_number in range(len(lab)):
-            tuple_list = [tuple for tuple in path if tuple[0] == line_number]
-            for element in tuple_list:
-                lab[line_number] = replace_at_index(lab[line_number], "X", element[1])
+        for line_index, line in enumerate(lab):
+            result_tuple_list = [path_tuple for path_tuple in path]
+            for element in result_tuple_list:
+                if element[0] == line_index:
+                    line = replace_at_index(line, "X", element[1])
+            print(line)
     else:
-        columns_counter = len(lab[0])
-        columns_counter_string = " "
-        for i in range(1, columns_counter + 1):
-            columns_counter_string += str(i)
-            if i == columns_counter + 1:
-                columns_counter_string += " "
-        lab.insert(0, columns_counter_string)
-        lab.append(columns_counter_string)
-        for index in range(1, len(lab) - 1):
-            lab[index] = str(index) + lab[index] + str(index)
-    for line in lab:
-        print(line)
+        number_of_columns = len(lab[0])
+        column_label = "".join(str(i + 1) for i in range(number_of_columns))
+        lab.insert(0, " " + column_label)
+        lab.append(" " + column_label)
+        for index, line in enumerate(lab):
+            if index not in [0, len(lab) - 1]:
+                lab[index] = "".join([str(index), lab[index], str(index)])
+            print(lab[index])
 
 
 def replace_at_index(labyrinth_line: str, replacement: str, index: int) -> str:
@@ -34,6 +32,16 @@ def prompt_integer(message: str) -> int:
     return x
 
 
+def check_if_in_labyrinth_and_traversable(lab: list[str], point: tuple[int, int]) -> bool:
+    row_counter = len(lab) - 2
+    column_counter = len(lab[0]) - 2
+    if point[0] > row_counter or point[1] > column_counter:
+        return False
+    if not is_traversable(lab, point) or not is_traversable(lab, point):
+        return False
+    return True
+
+
 def prompt_user_for_location(name: str) -> tuple[int, int]:
     row = int(prompt_integer("Row of " + name + ": "))
     column = int(prompt_integer("Column of " + name + ": "))
@@ -41,23 +49,12 @@ def prompt_user_for_location(name: str) -> tuple[int, int]:
 
 
 def bfs(lab: list[str], start: tuple[int, int], end: tuple[int, int]) -> list[tuple[int, int]]:
-    # check if start and end tuple exist in labyrinth:
-    row_counter = len(lab) - 2
-    column_counter = len(lab[0]) - 2
-    if start[0] > row_counter or end[0] > row_counter or start[1] > column_counter or end[1] > column_counter:
-        return []
-
-    # check if start and end are traversable
-    if not is_traversable(lab, start) or not is_traversable(lab, end):
-        return []
-
     already_visited_set = set()
     already_visited_set.add(start)
-    goal = tuple(end)
     path = deque([start])
     possible_moves = ((0, 1), (0, -1), (1, 0), (-1, 0))
     dict_of_parents = {}
-    # check if deque empty
+
     while path:
         current = path.popleft()
         if current == end:
@@ -69,11 +66,11 @@ def bfs(lab: list[str], start: tuple[int, int], end: tuple[int, int]) -> list[tu
             solution_path.append(start)
             solution_path.reverse()
             return solution_path
-        for possible_move in possible_moves:
-            possible_row = int(current[0]) + int(possible_move[0])
-            possible_column = int(current[1]) + int(possible_move[1])
-            neighbour = tuple((possible_row, possible_column))
-            if is_traversable(lab, neighbour) and neighbour not in already_visited_set:
+        for move_to_consider in possible_moves:
+            row_to_consider = int(current[0]) + int(move_to_consider[0])
+            column_to_consider = int(current[1]) + int(move_to_consider[1])
+            neighbour = tuple((row_to_consider, column_to_consider))
+            if neighbour not in already_visited_set and is_traversable(lab, neighbour):
                 path.append(neighbour)
                 already_visited_set.add(neighbour)
                 dict_of_parents[neighbour] = current
@@ -84,7 +81,7 @@ def bfs(lab: list[str], start: tuple[int, int], end: tuple[int, int]) -> list[tu
 
 def is_traversable(lab: list[str], location: tuple[int, int]) -> bool:
     row, column = location
-    if lab[row][column] == chr(32):
+    if lab[row][column] == '\u0020':
         return True
     else:
         return False
@@ -93,21 +90,28 @@ def is_traversable(lab: list[str], location: tuple[int, int]) -> bool:
 if __name__ == '__main__':
     space = '\u0020'
     wall = '\u2588'
-    labyrinth = [wall + wall + wall + space + space + space + wall + wall + wall + wall,
-                 wall + wall + space + space + wall + space + space + space + wall + wall,
-                 wall + wall + wall + wall + wall + space + wall + space + wall + wall,
-                 wall + wall + space + space + space + space + wall + space + wall + wall,
-                 wall + wall + space + wall + wall + wall + wall + space + space + wall,
-                 wall + wall + space + space + space + space + space + space + space + wall,
-                 wall + space + space + wall + space + wall + wall + space + wall + wall,
-                 wall + space + wall + space + space + wall + space + space + wall + wall,
-                 wall + space + space + wall + space + space + space + wall + wall + wall]
+    labyrinth = [f"{wall}{wall}{wall}{space}{space}{space}{wall}{wall}{wall}{wall}",
+                 f"{wall}{wall}{space}{space}{wall}{space}{space}{space}{wall}{wall}",
+                 f"{wall}{wall}{wall}{wall}{wall}{space}{wall}{space}{wall}{wall}",
+                 f"{wall}{wall}{space}{space}{space}{space}{wall}{space}{wall}{wall}",
+                 f"{wall}{wall}{space}{wall}{wall}{wall}{wall}{space}{space}{wall}",
+                 f"{wall}{wall}{space}{space}{space}{space}{space}{space}{space}{wall}",
+                 f"{wall}{space}{space}{wall}{space}{wall}{wall}{space}{wall}{wall}",
+                 f"{wall}{space}{wall}{space}{space}{wall}{space}{space}{wall}{wall}",
+                 f"{wall}{space}{space}{wall}{space}{space}{space}{wall}{wall}{wall}"]
 
     print_labyrinth(labyrinth)
 
-    start_row, start_column = prompt_user_for_location("start")
-    end_row, end_column = prompt_user_for_location("end")
+    starting_point = ()
+    end_point = ()
+    valid_locations = [False, False]
+    while not valid_locations[0]:
+        starting_point = prompt_user_for_location("start")
+        valid_locations[0] = check_if_in_labyrinth_and_traversable(labyrinth, starting_point)
+    while not valid_locations[1]:
+        end_point = prompt_user_for_location("end")
+        valid_locations[1] = check_if_in_labyrinth_and_traversable(labyrinth, end_point)
 
-    res_path = bfs(labyrinth, (start_row, start_column), (end_row, end_column))
+    res_path = bfs(labyrinth, starting_point, end_point)
 
     print_labyrinth(labyrinth, res_path)
